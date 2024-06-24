536dd34cd45e979419588c02a26612e2770e4251
Shantanu Kumar Mahanta <121172938+shantanukumar-dev@users.noreply.github.com>
2024-06-24 17:48:06 +0530

    Add files via upload
    This is the third session on jaavascript. in this part we desscussed about dtatypes in javascript.
    There are two types of datatypes in javascript.
    1.primitives
    ->number
    ->string
    ->boolean
    ->null
    ->undefined
    ->bigint
    ->symbol
    2.non-primitives
    ->array
    ->object
    ->function

---
+++ script.js
@@ -0,0 +1,61 @@
+//  In javascript There are two of datatypes are There
+//            1.primitive datatypes
+//            2.Non-primirive datatypes
+//Incase of primitive data types
+
+//1.Number
+// *Number are Written with or without decimal.(Any Real number)
+let num = 500;
+console.log(num);
+console.log(typeof num);
+
+let num1 = 8.9;
+console.log(num1);
+console.log(typeof num1);
+
+let num2 = -10;
+console.log(num2);
+console.log(typeof num2);
+
+//  2.String
+
+let s1 = "shantnu";
+console.log(s1);
+console.log(typeof s1);
+
+let s2 = 'shantanu kumar';
+console.log(s2);
+console.log(typeof s2);
+
+
+//  3.boolean
+// *That can be either true and false
+const a = true;
+console.log(a);
+console.log(typeof a);
+
+const b = false;
+console.log(b);
+console.log(typeof b);
+
+
+// 4.null
+// * null means nothing that does not exist.
+let x = null;
+console.log(typeof x);//The type of the null value is object.(Remember)
+
+//  5.undefined
+let z;
+console.log(typeof z);
+//  6.bigint
+//let  big=212345678900987654321234567890123456789012345678901234567891234567890.......n;
+console.log(typeof big);
+//  7.symbol
+// *Minimum use in jvascript
+
+// Incase of non-primitive datatypes
+// 1.Array
+// 2.object
+// 3.Function
+// we will disscuss further separately.
+// Thank you for watvhing this. lets follow me on Github.
